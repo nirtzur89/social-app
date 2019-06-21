515a0e78c7f5837a50e67bf839181f7a5580fd63
nirtzur <nirtzur89@gmail.com>
2019-06-21 19:16:03 +0200

    add and delete comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -142,7 +142,7 @@ router.put("/like/:id", auth, async (req, res) => {
     }
 });
 
-// PUT api/posts/like/:id
+// PUT api/posts/unlike/:id
 // adding like to post
 // Private
 router.put("/unlike/:id", auth, async (req, res) => {
@@ -171,6 +171,77 @@ router.put("/unlike/:id", auth, async (req, res) => {
     }
 });
 
+// POST api/posts/comment/:id
+// create comment on a post
+// Private
+router.post('/comment/:id',
+    [auth,
+        [check('text', 'Text is required').not().isEmpty()]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password')
+            const post = await Post.findById(req.params.id)
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            }
+
+            post.comments.unshift(newComment)
+
+            await post.save()
+            res.json(post.comments)
+
+        } catch (error) {
+            console.error(error.maessage)
+            res.status(500).send('server error')
+
+        }
+    })
+
+// DELETE api/posts/comment/:id/:comment_id
+// delete comment on a post
+// Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        //pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+        //make sure comment exists
+        if (!comment) {
+            return res.status(404).json({
+                msg: 'comment doesnt exist'
+            })
+        }
+        //check user
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                msg: 'user not authorized'
+            })
+        }
+
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id)
+
+        post.comments.splice(removeIndex, 1)
+
+        await post.save()
+        res.json(post.comments)
+
+    } catch (error) {
+        console.error(error.maessage)
+        res.status(500).send('server error')
+    }
+});
 
 
 module.exports = router
