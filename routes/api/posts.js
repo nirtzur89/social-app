d7955593c715bc9970fc726b8278aab3d793b78b
nirtzur <nirtzur89@gmail.com>
2019-06-21 18:35:02 +0200

    like and unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -79,7 +79,7 @@ router.get("/:id", auth, async (req, res) => {
 });
 
 // DELETE api/posts/:id
-// get all post
+// remove post
 // Private
 router.delete("/:id", auth, async (req, res) => {
     try {
@@ -113,5 +113,64 @@ router.delete("/:id", auth, async (req, res) => {
         res.status(500).send('server error')
     }
 });
+
+// PUT api/posts/like/:id
+// adding like to post
+// Private
+router.put("/like/:id", auth, async (req, res) => {
+    try {
+
+        const post = await Post.findById(req.params.id)
+
+        //check if user already liked this post
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({
+                msg: 'Post already liked'
+            })
+        }
+
+        post.likes.unshift({
+            user: req.user.id
+        })
+
+        await post.save()
+        res.json(post.likes)
+
+    } catch (error) {
+        console.error(error.maessage)
+        res.status(500).send('server error')
+    }
+});
+
+// PUT api/posts/like/:id
+// adding like to post
+// Private
+router.put("/unlike/:id", auth, async (req, res) => {
+    try {
+
+        const post = await Post.findById(req.params.id)
+
+        //check if user already liked this post
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({
+                msg: 'Post has not yet been liked'
+            })
+        }
+
+        //get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id)
+
+        post.likes.splice(removeIndex, 1)
+
+        await post.save()
+        res.json(post.likes)
+
+    } catch (error) {
+        console.error(error.maessage)
+        res.status(500).send('server error')
+    }
+});
+
+
 
 module.exports = router
